06f71bed515edcb53b28b022f2b0c2c28baaba0f
mehed-hasan <mehedi.hasan@intelli.global>
2022-07-19 18:55:32 +0600

    Navbar and hero

---
+++ assets/js/preloader.js
@@ -0,0 +1,7 @@
+// Code for preloader will be disappeared after all contents being loaded ----------------------------------------------------------*****
+$(window).on('load', function () {
+    $("#preloader ").fadeOut(500); 
+    $("#main").css({'overflow':'unset'}); 
+  
+  });
+  
